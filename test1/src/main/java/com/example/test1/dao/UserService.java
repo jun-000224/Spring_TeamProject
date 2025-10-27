5e5371d73fde652888e4816b13c083cde2bbd028
HTML <dongjun032061@gmail.com>
2025-10-27 11:48:03 +0900

    첫 커밋입니다.

---
+++ test1/src/main/java/com/example/test1/dao/UserService.java
@@ -0,0 +1,23 @@
+package com.example.test1.dao;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.test1.mapper.UserMapper;
+import com.example.test1.model.User;
+
+@Service
+public class UserService{
+	
+	@Autowired
+	UserMapper userMapper;
+	
+	public HashMap<String, Object> userLogin(HashMap<String, Object> map) {
+		// TODO Auto-generated method stub
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+			User user = userMapper.userLogin(map);
+		return resultMap;
+	}
+}
