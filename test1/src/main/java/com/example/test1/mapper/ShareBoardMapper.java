2a38baa4f8ceee8ac4e5080c8b7b9936e20a3667
hee <zcgy432@gmail.com>
2025-10-28 18:22:03 +0900

    share-board-view page

---
+++ test1/src/main/java/com/example/test1/mapper/ShareBoardMapper.java
@@ -0,0 +1,19 @@
+package com.example.test1.mapper;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.test1.model.Board;
+import com.example.test1.model.Comment;
+import com.example.test1.model.Share;
+import com.example.test1.model.User;
+
+@Mapper
+public interface ShareBoardMapper {
+
+	// 상세보기
+	List<Share> sharInfo(HashMap<String , Object> map);
+
+}
