684918500d08e42e4fe41e6c973e0e19a6042df6
3y5adf <duskfex@gmail.com>
2025-11-18 09:35:28 +0900

    Add files via upload
    클라우드 db/seq 백업

---
+++ springTeamProject_seq_Backup_20251118.sql
@@ -0,0 +1,53 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 화요일-11월-18-2025   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence AREA_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."AREA_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence BOARD_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."BOARD_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 265 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence COMMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."COMMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 338 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence FAV_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."FAV_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 109 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence MEDIA_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."MEDIA_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 162 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence POI_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."POI_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 826 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence REPORT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."REPORT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 139 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence RES_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."RES_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 229 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence REVIEW_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."REVIEW_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 46 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence THEME_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMIN"."THEME_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
